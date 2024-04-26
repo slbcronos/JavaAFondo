2f8e9d88374a2be94934694c7508c31828fb5e10
Salvador LB <39510977+slbcronos@users.noreply.github.com>
2024-04-25 20:41:57 -0600

    Descripcion del commit

---
+++ src/TestFecha.java
@@ -0,0 +1,29 @@
+import com.slcomputacion.clases.Fecha;
+
+public class TestFecha {
+    public static void main(String[] args) {
+        Fecha f = new Fecha();
+
+        f.setDia(2);
+        f.setMes(3);
+        f.setAnio(1983);
+
+        System.out.println(f.toString());
+
+        Fecha f2 = new Fecha();
+        f2.setDia(2);
+        f2.setMes(3);
+        f2.setAnio(1983);
+
+        System.out.println(f2.toString());
+
+        if (f.toString().equals(f2.toString())){
+            System.out.println("Las fechas son iguales mai");
+        }else {
+            System.out.println("Las fechas son diferenes perro");
+        }
+
+
+
+    }
+}
---
+++ src/com/slcomputacion/clases/Fecha.java
@@ -0,0 +1,44 @@
+package com.slcomputacion.clases;
+
+public class Fecha {
+    private int dia;
+    private int mes;
+    private  int anio;
+
+    //get y set
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    //Metodos
+
+
+
+
+    @Override
+    public String toString() {
+        return "Fecha: "+dia +"/"+mes+"/"+anio;
+        //return "Fecha: "+dia +"/"+mes+"/"+anio +"(dd/mm/aaaa)";
+    }
+}
